1ee7d594b8ce9a7fc57ee4ef88c4a0dd8e1a4bcc
Emily Richardson <richardson96@gmail.com>
2020-04-23 20:30:29 -0500

    finished studio with 2/3 bonus

--- src/Studios/Studio2_CountingCharacters/CountCharacters.java
+++ src/Studios/Studio2_CountingCharacters/CountCharacters.java
@@ -1,4 +1,29 @@
 package Studios.Studio2_CountingCharacters;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class CountCharacters {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a sentence: ");
+        String sentence = input.nextLine();
+
+//        String exampleSentence = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+        char[] charactersInSentence = sentence.toLowerCase().toCharArray();
+
+        HashMap<Character, Integer> characterCountList = new HashMap<>();
+
+        for (char character: charactersInSentence) {
+            if (characterCountList.containsKey(character)) {
+                characterCountList.put(character, characterCountList.get(character) + 1);
+            } else {
+                  characterCountList.put(character, 1);
+            }
+        }
+
+        System.out.println(characterCountList);
+    }
 }
